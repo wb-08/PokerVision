d60c98f5d5db1aacbde14e620bf729ff8975ab4a
wb-08 <beschten.vadimka@gmail.com>
2023-05-13 16:21:23 +0300

    add tests to dealer button

--- unittests/TestTableRecognition.py
+++ unittests/TestTableRecognition.py
@@ -1,5 +1,5 @@
 import unittest
-from scripts.table_recognition import detect_hero_step, detect_hero_cards, detect_table_cards, find_total_pot
+from scripts.table_recognition import detect_hero_step, detect_hero_cards, detect_table_cards, find_total_pot, get_dealer_button_position
 from scripts.utils import read_config_file, load_images
 
 cfg = read_config_file('../scripts/config.yaml')
@@ -32,6 +32,11 @@ class TestTableRecognition(unittest.TestCase):
             with self.subTest("TestTotalPot Incorrect detection in the image", filename=filename):
                 self.assertEqual(find_total_pot(image, cfg), test_cfg['total_pot'][filename])
 
+    def test_dealer_button_position(self):
+        images, file_names = load_images(test_cfg['paths']['dealer_button_position'])
+        for image, filename in zip(images, file_names):
+            with self.subTest("TestTotalPot Incorrect detection in the image", filename=filename):
+                self.assertEqual(get_dealer_button_position(image, cfg), test_cfg['dealer_button_position'][filename])
 
 
 if __name__ == '__main__':
